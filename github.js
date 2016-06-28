0d5a3e61caa9828ef885f343f6b7ed9820b9967d
Caroline Linde <Caroline Linde>
2016-06-28 11:43:38 +0200

    Fertige Hausarbeit

---
+++ github.js
@@ -0,0 +1,28 @@
+/**
+ * Created by vendr on 16.06.2016.
+ */
+
+/** Aufruf DataTable aus json-Datei (kleines d, großes T!!!)*/
+$("#befehle").dataTable({
+    /**Aufrufen der Daten für die Tabelle*/
+    "ajax" : "befehle.json",
+});
+
+/** Aufruf Tooltipster*/
+$(".tooltip").tooltipster();
+
+$("#clone").tooltipster({
+    content: $("#cloneContent").detach()
+});
+$("#add").tooltipster({
+    content: $("#addContent").detach()
+});
+$("#commit").tooltipster({
+    content: $("#commitContent").detach()
+});
+$("#push").tooltipster({
+    content: $("#pushContent").detach()
+});
+$("#pull").tooltipster({
+    content: $("#pullContent").detach()
+})
